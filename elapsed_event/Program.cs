0d9dcbab8d133b4b73305628fc4dbdf0655f290a
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-22 22:45:58 +1100

    Initial commit

---
+++ elapsed_event/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * C# Program to Illustrate Elapsed Event
+ */
+using System;
+using System.Timers;
+public class Program
+{
+    private static System.Timers.Timer Tim;
+    public static void Main()
+    {
+        Tim = new System.Timers.Timer(10);
+        Tim.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+        Tim.Interval = 1000;
+        Tim.Enabled = true;
+        Console.WriteLine("Press Any Key to Exit else Elapsed Event will be Raised ");
+        Console.ReadLine();
+    }
+}
